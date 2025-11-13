7d1163b80d96e826c4b9fe9b6d495744ac3108e1
Nikolaos Argyropoulos <t8240012@aueb.gr>
2025-11-13 23:50:21 +0000

    Add Return feature on menu and add readable view

--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,8 +1,16 @@
+import java.text.NumberFormat;
+import java.util.Locale;
 // Class for operating the View option of the menu
 public class View {
     public static void viewGovBudget() {
+        double mbudg;
+        NumberFormat df = NumberFormat.getNumberInstance(Locale.US);//make number readable
+        df.setMaximumFractionDigits(2);
+        String readable;
         for (Ministry m : CreatingMinistries.ministries) {
-            System.out.println(m.getMinistryName() + ": " + m.getBudget());
+            mbudg=m.getBudget();
+            readable = df.format(mbudg); //Caution readble variable is String it is used only for readable print in View
+            System.out.println(m.getMinistryName() + ": " + readable);
         }
     }
 }
