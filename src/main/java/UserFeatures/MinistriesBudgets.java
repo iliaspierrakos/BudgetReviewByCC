39ba54d365904899f4d94748132b0fd8fffe72b8
Konstantinos Patsouris <t8240111@aueb.gr>
2025-11-24 11:42:40 +0000

    Add comments to MinistriesBudgets class

--- src/main/java/UserFeatures/MinistriesBudgets.java
+++ src/main/java/UserFeatures/MinistriesBudgets.java
@@ -5,7 +5,23 @@ import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.*;
 
+/**
+ * The MinistriesBudgets class processes budget data from an external text file and
+ * proceeds into extracting relevant lines into a CSV format.
+ * The procedure is based on regular expressions 
+ * to filter the records and pick out the ones that belong to specific ministries
+ * and have certain expense codes. 
+ */
 public class MinistriesBudgets {
+    
+    /**
+     * Executes the process of reading the input file, filtering the data,
+     * and creating the output file, in accordance with the specifications.
+     * 
+     * * The method uses **try-with-resources** to ensure automatic
+     * management of resources (BufferedReader, BufferedWriter) and handles any
+     * input/output errors ({@code IOException}).
+     */
     public void budget() {
         Path inputFile = Path.of("NecessaryFilesAndData/BudgetReview2025.txt");
         Path outputFile = Path.of("NecessaryFilesAndData/MinistriesBudgets.csv");
