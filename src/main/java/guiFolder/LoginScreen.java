0753efb33d9147092a2636a4d4b483656929b054
Ilias Pierrakos <t8240114@aueb.gr>
2025-12-03 17:17:47 +0200

    First class for Graphics

---
+++ src/main/java/guiFolder/LoginScreen.java
@@ -0,0 +1,64 @@
+package guiFolder;
+
+import UserManagement.UserManager;
+
+import java.lang.classfile.Label;
+
+import UserManagement.User;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class LoginScreen {
+    private final UserManager userManager;
+    public LoginScreen(UserManager userManager) {
+        this.userManager = userManager;
+    }
+    public void show(Stage stage) {
+        Label title = new Label("Login");
+        title.setStyle("-fx-font-size: 22px; -fx-font-weight: bold;");
+        TextField usernameField = new TextField();
+        usernameField.setPromtText("Username");
+        PassowrField passwordField = new PasswordField();
+        passwordField.setPromtText("Password");
+        Label errorLabel = new Label();
+        errorLabel.setStyle("-fx-text-fill: red;");
+        Button loginButton = new Button("Login");
+        loginButton.setMinWidth(120);
+        Button backButton = new Button("Back");
+        backButton.setMinWidth(120);
+        
+        loginButton.setOnAction(e -> {
+            String username = usernameField.getText().trim();
+            String password = passwordField.getText().trim();
+            if (username.isEmpty() || password.isEmpty()) {
+                errorLabel.setText("Fields cannot be empty!");
+                return;
+            }
+            User loggedIn = userManager.loginUser(username, password);
+            if (loggedIn == null) {
+                errorLabel.setText("Invalid username or password.");
+            } else {
+                errorLabel.setText("");
+                MenuScreen menu = new MenuScreen(loggedIn, userManager);
+                menu.show(stage);
+            }
+        });
+        backButton.setOnAction(e -> {
+            new StartMenuScreen(userManager).show(stage);
+        });
+        VBox layout = new VBox(
+            15, tittle, usernameField, passwordField, loginButton, backButton, errorLabel);
+        layout.setAlignment(Pos.CENTER);
+        layout.setPadding(new Insets(20));
+        Scene = new Scene(layout, 350, 260);
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.show();    
+    }
+}
+
