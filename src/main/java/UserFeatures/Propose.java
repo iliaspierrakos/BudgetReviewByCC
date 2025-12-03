97c3911012b6cce87069bbaafd14c1d20ae6b7da
Anastasis Marathakis <t8240075@aueb.gr>
2025-12-03 20:43:12 +0000

    Complete Propose class Minor changes in the feature menu and the BudgetReviewMain class

--- src/main/java/UserFeatures/Propose.java
+++ src/main/java/UserFeatures/Propose.java
@@ -1,27 +1,30 @@
 package UserFeatures;
-import java.util.Scanner;
-import UserManagement.User;
+/**
+ * This is a class that creates the proposals the ministers make.
+ * Each minister has a .txt proposal that are saved in a specific 
+ * folder where the governor will have access and take action.
+ */
 import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Propose {
-    public static void editProposal(User user,String ministryName, double previousBudget, double newBudget) {
-        if (user.getRole() == User.Role.GOVERNOR || user.getRole() == User.Role.CITIZEN) {
-            return;
-        }
-        Scanner scanner = new Scanner(System.in);
-        FileWriter fw = null;
-        PrintWriter pw = null;
-        try {
-            fw = new FileWriter("NecessaryFilesAndData/" + user.getUsername() + " .txt", true);
-            pw = new PrintWriter(fw);
-            pw.println(ministryName + " previous budget: " + Ministry.getFormattedBudget(previousBudget));
-            pw.println(ministryName + " new budget: " + Ministry.getFormattedBudget(newBudget));
-
-            pw.close();
-            fw.close();
-        } catch(IOException e) {}
-
+    public void editProposal(String username) {// taking the user's username as a parameter to create the unique file 
+            FileWriter fw = null;
+            PrintWriter pw = null;
+            try {
+                fw = new FileWriter("NecessaryFilesAndData/ProposalsFromMinisters/" + username + ".txt", true);
+                pw = new PrintWriter(fw);
+                System.out.println("Editing budget...");
+                Edit proposeEdit = new Edit();
+                proposeEdit.collectData(); // calling the collectData method enables editing
+                //System.out.println("Number of edits in history: " + Edit.history.editList.size());
+                for (Edit e : Edit.history.editList) {
+                    //System.out.println("this works");
+                    pw.println(e.toString()); // calling the edit objects toString
+                }
+                pw.close();
+                fw.close();
+            } catch(IOException e) {}
     }
 }
