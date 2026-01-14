c407f9860c29a9afce24d208fc3d529ef5f09347
Myrto Talantzi <t8240138@aueb.gr>
2026-01-15 01:55:05 +0200

    Update Governor class

--- src/main/java/UserManagement/Governor.java
+++ src/main/java/UserManagement/Governor.java
@@ -1,37 +1,34 @@
 package UserManagement;
 
 /**
- * Represents the Governor (Prime Minister) in the system.
- * This class extends User and sets the role to GOVERNOR.
- * 
+ * Represents the Governor (Prime Minister) in the system. This class extends User and sets the role
+ * to GOVERNOR.
+ *
  * A Governor has full access to regional and national data.
  */
 public class Governor extends User {
 
-    /**
-     * Constructor for creating a Governor.
-     * 
-     * @param username the username of the Governor
-     * @param password the password of the Governor
-     */
-    public Governor(String username, String password) {
-        super(username, password, Role.GOVERNOR);
-    }
+  /**
+   * Constructor for creating a Governor.
+   *
+   * @param username the username of the Governor
+   * @param password the password of the Governor
+   */
+  public Governor(String username, String password) {
+    super(username, password, Role.GOVERNOR);
+  }
 
-    /**
-     * Returns a string representation of the Governor.
-     * Includes only the username.
-     * 
-     * @return a string describing the Governor
-     */
-    @Override
-    public String toString() {
-        return "Governor: " + getUsername();
-    }
+  /**
+   * Returns a string representation of the Governor. Includes only the username.
+   *
+   * @return a string describing the Governor
+   *
+   */
+  @Override
+  public String toString() {
+    return "Governor: " + getUsername();
+  }
 
-    // Additional Governor-specific methods can be added here, 
-    // e.g., approve budgets, manage ministries, etc.
+  // Additional Governor-specific methods can be added here,
+  // e.g., approve budgets, manage ministries, etc.
 }
-
-
-
