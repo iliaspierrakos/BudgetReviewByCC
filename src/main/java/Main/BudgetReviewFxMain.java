4a41870cf577ef9b710acbf85f0f66e6f89ce2df
Myrto Talantzi <t8240138@aueb.gr>
2026-01-15 01:54:47 +0200

    Update BudgetReviewFxMain class

--- src/main/java/Main/BudgetReviewFxMain.java
+++ src/main/java/Main/BudgetReviewFxMain.java
@@ -5,22 +5,16 @@ import guiFolder.StartMenuScreen;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-
 public class BudgetReviewFxMain extends Application {
+  private static UserManager userManager;
 
-    private static UserManager userManager;
+  public static void main(String[] args) {
+    userManager = new UserManager();
+    launch(args); // JavaFX launch
+  }
 
-    public static void main(String[] args) {
-        userManager = new UserManager();
-        launch(args);   // 🚀 JavaFX launch
-    }
-
-    @Override
-    public void start(Stage stage) {
-
-     
-
-       
-        new StartMenuScreen(userManager).show(stage);
-    }
+  @Override
+  public void start(Stage stage) {
+    new StartMenuScreen(userManager).show(stage);
+  }
 }
