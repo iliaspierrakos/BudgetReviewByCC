5a2106b3953caa6eb5698761735a580fd4fd34c7
Myrto Talantzi <t8240138@aueb.gr>
2026-01-15 01:55:04 +0200

    Update Citizen class

--- src/main/java/UserManagement/Citizen.java
+++ src/main/java/UserManagement/Citizen.java
@@ -1,33 +1,36 @@
 package UserManagement;
 
 /**
- * Represents a Citizen in the system.
- * This class extends User and sets the role to CITIZEN.
- * 
+ * Represents a Citizen in the system. This class extends User and sets the role to CITIZEN.
+ *
+ * <p>
  * Each Citizen has a username and password.
  */
 public class Citizen extends User {
 
-    /**
-     * Constructor for creating a Citizen.
-     * 
-     * @param username the username of the citizen
-     * @param password the password of the citizen
-     */
-    public Citizen(String username, String password) {
-        super(username, password, Role.CITIZEN);
-    }
+  /**
+   * Constructor for creating a Citizen.
+   *
+   * @param username the username of the citizen
+   * @param password the password of the citizen
+   *
+   */
 
-    /**
-     * Returns a string representation of the Citizen.
-     * Includes only the username.
-     * 
-     * @return a string describing the Citizen
-     */
-    @Override
-    public String toString() {
-        return "Citizen: " + getUsername();
-    }
+  public Citizen(String username, String password) {
+    super(username, password, Role.CITIZEN);
+  }
 
-    // Additional Citizen-specific methods can be added here
+  /**
+   * Returns a string representation of the Citizen. Includes only the username.
+   *
+   * @return a string describing the Citizen
+   *
+   */
+
+  @Override
+  public String toString() {
+    return "Citizen: " + getUsername();
+  }
+
+  // Additional Citizen-specific methods can be added here
 }
